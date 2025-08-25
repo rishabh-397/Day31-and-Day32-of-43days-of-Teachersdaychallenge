437f8c65dccb16819707cf715b5710d9d000f077
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-25 21:14:41 +0530

    C. Registration system.cpp

---
+++ C. Registration system.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    unordered_map<string, int> database;
+    
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        
+        if (database.find(s) == database.end()) {
+            database[s] = 0;
+            cout << "OK" << endl;
+        } else {
+            database[s]++;
+            cout << s << database[s] << endl;
+        }
+    }
+    
+    return 0;
+}
