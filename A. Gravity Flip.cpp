2e92550efe8a54b46e2d2e1beb5d8b33208f61c9
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-25 21:14:05 +0530

    A. Gravity Flip.cpp

---
+++ A. Gravity Flip.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> cubes(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> cubes[i];
+    }
+    
+    sort(cubes.begin(), cubes.end());
+    
+    for (int i = 0; i < n; i++) {
+        cout << cubes[i] << " ";
+    }
+    
+    return 0;
+}
